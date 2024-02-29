35ff9bd632bba9d6e40423267fb5a035d041cb57
257helloWorld <adityakadam567@gmail.com>
2024-02-29 15:10:46 +0530

    Updated: Edit Profile UI

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -8,9 +8,11 @@ import {
   IonHeader,
   IonIcon,
   IonImg,
+  IonInput,
   IonMenuButton,
   IonPage,
   IonText,
+  IonTextarea,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
@@ -35,14 +37,65 @@ const Tab: React.FC = () => {
               <IonBackButton></IonBackButton>
             </IonButtons>
             <IonTitle style={{ textAlign: "center" }}>Edit Profile</IonTitle>
-            <IonButtons slot="end">
+            {/* <IonButtons slot="end">
               <div style={{ marginRight: "50px" }}></div>
-            </IonButtons>
+            </IonButtons> */}
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          <div className="photo">
-            {/* <IonImg src={camera} className="camera"></IonImg> */}
+          <div className="editProfile_Photo">
+            <div className="eP_photoBg">
+              <IonImg src={camera} className="eP_camera"></IonImg>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "grid",
+              padding: "10px",
+              marginLeft: "20px",
+              marginRight: "20px",
+              columnGap: "15px",
+              gridTemplateColumns: "auto auto",
+            }}
+          >
+            <p className="eP_Font">First Name</p>
+            <p className="eP_Font">Last Name</p>
+            <IonInput
+              class="eP_Name"
+              placeholder="First Name"
+              maxlength={20}
+            ></IonInput>
+            <IonInput
+              class="eP_Name"
+              placeholder="Last Name"
+              maxlength={20}
+            ></IonInput>
+
+            <p className="eP_Font" id="eP_colspan">
+              Email Address
+            </p>
+            <IonInput
+              class="eP_Name"
+              id="eP_colspan"
+              placeholder="example@abc.com"
+              maxlength={50}
+            ></IonInput>
+
+            <p className="eP_Font" id="eP_colspan">
+              Bio
+            </p>
+            <IonTextarea
+              placeholder="Write about yourself"
+              class="custom"
+              id="eP_height"
+              // helperText="Helper text"
+              counter={true}
+              maxlength={85}
+            ></IonTextarea>
+            {/* <p className="count">/85</p> */}
+            <IonButton className="eP_Button" id="eP_colspan">
+              Save
+            </IonButton>
           </div>
         </IonContent>
       </IonPage>
