4082dafa8fc29d0abef750589838269e7da58089
Aditya Kadam <110030634+257helloworld@users.noreply.github.com>
2024-09-28 10:47:33 +0530

    Minor Changes

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -4,12 +4,6 @@ import { getAuth } from "firebase/auth";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyD9X-f3IAx-JRgeobDBRtMKRUdTqvZnC0A",
-  // authDomain: "codrive-fec10.appspot.com",
-  // projectId: "codrive-fec10",
-  // messagingSenderId: "874102047441",
-  // appId: "1:874102047441:android:1494b77a8ccab9b3f83bf0",
-  // measurementId: "G-CF4FDKC63F",
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
